ec419e57c5eeeb2a94130bf744996d6c997891ac
Elior Zur <user1@Eliors-MacBook-Pro.local>
2019-11-27 15:17:06 +0200

    css-added

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -2,7 +2,7 @@
     <div class="list-item"> 
         <div class="list-item-details">
             <!-- <div class="labels"></div> -->
-            <div class="item-title">{{item.title}}</div>
+            <div class="list-item-title">{{item.title}}</div>
             <!-- <div class="tags"></div> -->
         </div>
         {{item.name}}
