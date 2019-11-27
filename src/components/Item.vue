00f9d17cceb2b4074ee6b2ad8edaa4ddfe4a3c5b
Elior Zur <user1@Eliors-MacBook-Pro.local>
2019-11-27 18:33:35 +0200

    somechanges

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list-item"> 
+    <div v-if="item" class="list-item" @click="openItemModal()"> 
         <div class="list-item-details">
             <!-- <div class="labels"></div> -->
             <div class="list-item-title">{{item.title}}</div>
@@ -10,8 +10,15 @@
 </template>
 
 <script>
+// import { bus } from '../main'
 
 export default {
     props:['item'],
+    components:{},
+    methods:{
+        openItemModal(){
+            console.log(this.item);
+        }
+    }
 }
 </script>
