adfc59cc727a6ab1719cd398b954d7e9bdda27d4
natan-git <54308928+natan-git@users.noreply.github.com>
2019-11-28 19:48:55 +0200

    add modal

--- src/components/BoardNavBar.vue
+++ src/components/BoardNavBar.vue
@@ -1,15 +1,26 @@
 <template>
-    <section class="board-nav-bar-container flex align-c ">
-        <h1 class="nav-bar-logo">taskflo</h1>
-        <input type="text" placeholder="search...">
-        <div class="search-icon-container">
-        <img src='@/assets/search-24.png'>
-        </div>
-    </section>
+  <section class="board-nav-bar-container flex align-c">
+    <h1 class="nav-bar-logo">taskflo</h1>
+    <input type="text" placeholder="search..." />
+    <div class="search-icon-container">
+      <img src="@/assets/search-24.png" />
+    </div>
+    <SideMenu :isOpen="isOpen">
+     
+    </SideMenu>
+  </section>
 </template>
 
 <script>
+import SideMenu from "../components/SideMenu.vue";
 export default {
-    
-}
+    data(){
+        return{
+            isOpen:true
+        }
+    },
+  components: {
+    SideMenu // Register your component
+  }
+};
 </script>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <p class="show-menu-btn">show menu</p>
+    <Slide right noOverlay>
+      <a id="home" href="#">
+        <span>Home</span>
+      </a>
+    </Slide>
+  </section>
+</template>
+
+
+<script>
+import { Slide } from "vue-burger-menu";
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods:{},
+  components: {
+    Slide // Register your component
+  }
+};
+</script>
+
