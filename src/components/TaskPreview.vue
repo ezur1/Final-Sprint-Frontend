0f6027fa211049ba1e72d03cc09f140743ca4902
Elior Zur <user1@Eliors-MacBook-Pro.local>
2019-11-28 19:50:08 +0200

    some feachers added to preview

--- src/components/TaskPreview.vue
+++ src/components/TaskPreview.vue
@@ -1,20 +1,43 @@
 <template>
     <section class="task-preview flex col">
         <div class="preview-header flex space-between">
-            <span>{{task.title}}</span>
-            <span>{{task.createdBy}}</span>
+            <div class="task-mid-info">
+                <h1>{{task.title}}</h1>
+                <span>{{task.createdBy}}</span>
+            </div>
+            
         </div>
         <div class="preview-body flex space-between">
             <div class="preview-main">
                <section class="description">
-                   
+                   <h3>Description</h3>
+                   <textarea name="description" @click="showDescBtns()"></textarea>
+                   <div v-if="isDescBtns" class="descBtns">
+                       <button>Save</button>
+                       <button>X</button>
+                   </div>
+               </section>
+               <section class="tasks">
+                   <h3>Tasks</h3>
+                   <div v-if="isDescBtns" class="descBtns">
+                       <button>Save</button>
+                       <button>X</button>
+                   </div>
                </section>
             </div>
-            <div class="preview-sidebar">
-                <div>yes</div>
-                <div>yes</div>
-                <div>yes</div>
-                <div>yes</div>
+            <div class="preview-sidebar flex col">
+                <div class="add-to-card flex col">
+                    <h3>ADD TO CARD</h3>
+                    <a href="#" title="Labels" class="prev-side-btn"><span>Labels</span></a>
+                    <a href="#" title="Check List" class="prev-side-btn"><span>Checklist</span></a>
+                    <a href="#" title="Due Date" class="prev-side-btn"><span>Due Date</span></a>
+                </div>
+                <div class="actions-on-card flex col">
+                    <h3>ACTIONS</h3>
+                    <a href="#" title="Move" class="prev-side-btn"><span>Move</span></a>
+                    <a href="#" title="Copy" class="prev-side-btn"><span>Copy</span></a>
+                    <a href="#" title="Delete" class="prev-side-btn"><span>Delete</span></a>
+                </div>
             </div> 
         </div>
     </section>
@@ -23,6 +46,17 @@
 <script>
 
 export default {
+    data(){
+        return{
+            isDescBtns:false
+        }
+    },
+    methods:{
+    showDescBtns(){
+        console.log("fuk");
+        this.isDescBtns=true;
+    }
+},
 computed:{
     task(){
         console.log(this.$store.getters.currTask);
