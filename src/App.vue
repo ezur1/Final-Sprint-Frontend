80d4335e99bbfcd41bd7513a2edcc7ea01587922
Elior Zur <user1@Eliors-MacBook-Pro.local>
2019-11-28 13:20:43 +0200

    push

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      push
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/signin">SignIn</router-link> |
       <router-link to="/about">About Us</router-link> -->
