5092ee4312a41e0711bede46e9774aaeeb5bdc0e
Ohad Leshem <ohadlesh@gmail.com>
2019-11-30 17:16:31 +0200

    fix

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -1,25 +1,29 @@
 import { httpService } from '../services/http.service.js';
 
 function query() {
-
-    return httpService.get('http://localhost:3000/boards')
+    // return httpService.get('http://localhost:3000/boards')
+    return httpService.get('http://localhost:3001/api/board')
 }
 
 function update(board) {
-    // console.log('edited._id', board._id);
-    return httpService.put(`http://localhost:3000/boards/${board._id}`, board)
+    // return httpService.put(`http://localhost:3000/boards/${board._id}`, board)
+    return httpService.put(`http://localhost:3001/api/board/${board._id}`, board)
 }
 
 function getById(id) {
-    return httpService.get(`http://localhost:3000/boards/${id}`)
+    console.log('the id is: ', id);
+    // return httpService.get(`http://localhost:3000/boards/${id}`)
+    return httpService.get(`http://localhost:3001/api/board/${id}`)
 }
 
 function remove(id) {
-    return httpService.delete(`http://localhost:3000/boards/${id}`)
+    // return httpService.delete(`http://localhost:3000/boards/${id}`)
+    return httpService.delete(`http://localhost:300/api/board/${id}`)
 }
 
 function add(added) {
-    return httpService.post(`http://localhost:3000/boards`, added)
+    // return httpService.post(`http://localhost:3000/boards`, added)
+    return httpService.post(`http://localhost:3001/api/boards`, added)
 }
 
 export default {
