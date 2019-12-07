868cdd952aa4de999ff5dbd0bcaec2f8e758d824
natan-git <54308928+natan-git@users.noreply.github.com>
2019-12-07 17:01:18 +0200

    added css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <mainNavBar/>
+    <div class="main-about-page-content">
+      <div class="about-header flex justify-center align-c ">
+        <div class="contact-us">
+          <h1>Contact us</h1>
+          <P>We're here to help and answer any question you might have.</P>
+          <P>We look forward to hearing from you.</P>
+        </div>
+      </div>
+    </div>
+    <div class="devs flex  align-c space-around">
+      <div class="ohad">
+        <img src="@/assets/img/ohad.jpg" alt="">
+        <h2>Ohad Leshem</h2>
+        <font-awesome-icon class="social-media-icon" icon="github-square" />
+      </div>
+      <div class="natan">
+        <img src="@/assets/img/natan.jpg" alt="">
+        <h2>Natan yagodayv</h2>
+        <font-awesome-icon icon="github" />
+      </div>
+      <div class="elior">
+        <img src="@/assets/img/elior.jpg" alt="">
+        <h2>Elior zur</h2>
+        <font-awesome-icon class="icon" icon="github" />
+
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import mainNavBar from '../components/MainNavBar.vue'
+export default {
+  components:{
+    mainNavBar
+  }
+  
+}
+</script>
